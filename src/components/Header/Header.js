260235408c33a71a071b7d3c64180d90f719837f
Marina Anisimova <anisimova.marina.n@gmail.com>
2021-02-22 21:57:33 +0300

    feature: дополнить header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,11 +18,12 @@ function Header (props) {
           className="header__link header__navigation-link">
             {/* {props.links[0]} */} Сохраненные Фильмы
           </Link>
+          <button className="header__link header__button">Войти</button>
         </div>
         <Link 
           // to={props.urlAdress} 
           className="header__link header__navigation-account">
-          {/* {props.account} */} Войти
+          {/* {props.account} */} Аккаунт
           <div className="header__navigation-account-icon" />
         </Link>
       </nav>
