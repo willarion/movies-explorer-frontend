7da2de5ae4b76288f225f34b9bfeee2145a0978a
Marina Anisimova <anisimova.marina.n@gmail.com>
2021-02-23 22:24:53 +0300

    feature: наверстать portfolio

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,6 +3,7 @@ import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
 import Techs from '../Techs/Techs';
 import AboutMe from '../AboutMe/AboutMe';
+import Portfolio from '../Portfolio/Portfolio';
 
 function Main(props) {
 
@@ -12,6 +13,7 @@ function Main(props) {
       <AboutProject />
       <Techs />
       <AboutMe />
+      <Portfolio />
     </main>
   )
 }
