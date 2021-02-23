8391b970725b76a75144ed08b6c8454e465b04c1
Marina Anisimova <anisimova.marina.n@gmail.com>
2021-02-23 17:44:10 +0300

    feature: наверстать AboutProject

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Promo from '../Promo/Promo';
+import AboutProject from '../AboutProject/AboutProject';
 
 function Main(props) {
 
   return (
     <main>
       <Promo />
+      <AboutProject />
     </main>
   )
 }
