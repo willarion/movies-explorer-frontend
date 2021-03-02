1aba338590b11f91c749f2a5a61481055f66e5e3
Marina Anisimova <anisimova.marina.n@gmail.com>
2021-03-02 17:17:03 +0300

    feature: настроить изменяемость внешнего вида навигации

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,43 +3,66 @@ import { Link } from 'react-router-dom';
 
 function Navigation (props) {
 
+  const loggedIn = true;
+
+  const [linkStyleMobile, setLinkStyleMobile] = React.useState("navigation__link");
+  const [linksGroupStyleMobile, setLinksGroupStyleMobile] = React.useState("navigation__links");
+
+  React.useEffect(() => {
+    if(loggedIn) {
+      setLinkStyleMobile("navigation__link_sided");
+      setLinksGroupStyleMobile("navigation__links_sided");
+    } else {
+      setLinkStyleMobile("navigation__link");
+      setLinksGroupStyleMobile("navigation__links");
+    }
+  }, [loggedIn]);
+
   return (
     <>
-      <div className="navigation__burger" onClick={props.toggleClass}>
-        <label for="burger" className={props.visible ? `navigation__burger-main-line navigation__burger-main-line_checked` : `navigation__burger-main-line`} />
+      <div className={loggedIn ? "navigation__burger navigation__burger_visible" : "navigation__burger"} onClick={props.toggleClass}>
+        <label for="burger" 
+        className={props.visible ? "navigation__burger-main-line navigation__burger-main-line_checked" : "navigation__burger-main-line"} 
+        />
       </div>
-      <nav className={props.visible ? `navigation navigation_visible` : `navigation`}>
+      <nav className={ loggedIn ? (props.visible ? "navigation_sided navigation_visible navigation" : "navigation_sided navigation") : "navigation" }>
         <Link 
           // to={props.urlAdress} 
-          className="navigation__account navigation__link">
+          className={ loggedIn ? "navigation__account navigation__link navigation__link-item_sided" : "navigation__link_invisible"}>
           {/* {props.account} */} Аккаунт
           <div className="navigation__account-icon" />
         </Link>
-
-        <ul className="navigation__links">
-          <li className="navigation__link-item">
+        <button className={loggedIn ?  "navigation__link_invisible": "navigation__button navigation__link"}>Войти</button>
+        <ul className={loggedIn ? "navigation__links navigation__links_sided" : "navigation__links"}>
+          <li className={loggedIn ? "navigation__link-item navigation__link-item_sided" :  "navigation__link-item"}>
             <Link 
             // to={props.urlAdress} 
-            className="navigation__link">
+            className={loggedIn ? (props.visible? "navigation__link navigation__link_sided" : "navigation__link") : "navigation__link_invisible"}>
               {/* {props.links[0]} */} Главная
             </Link> 
           </li>
-          <li className="navigation__link-item">
+          <li className={loggedIn ? "navigation__link-item navigation__link-item_sided" :  "navigation__link-item"}>
             <Link 
             // to={props.urlAdress} 
-            className="navigation__link">
+            className={loggedIn ? (props.visible? "navigation__link navigation__link_sided navigation__link_current" : "navigation__link") : "navigation__link_invisible"}>
               {/* {props.links[0]} */} Фильмы
             </Link>
           </li>
-          <li className="navigation__link-item">
+          <li className={loggedIn ? "navigation__link-item navigation__link-item_sided" :  "navigation__link-item"}>
             <Link 
             // to={props.urlAdress} 
-            className="navigation__link">
+            className={loggedIn ? (props.visible? "navigation__link navigation__link_sided" : "navigation__link") : "navigation__link_invisible"}>
               {/* {props.links[0]} */} Сохраненные Фильмы
             </Link>
           </li>
+          <li className={loggedIn ? "navigation__link-item navigation__link-item_sided" :  "navigation__link-item"}>
+            <Link 
+            // to={props.urlAdress} 
+            className={loggedIn ? "navigation__link_invisible" : "navigation__link"}>
+              {/* {props.links[0]} */} Регистрация
+            </Link>
+          </li>
         </ul>
-        {/* <button className="navigation__button navigation__link">Войти</button> */}
       </nav>
     </>
   )
