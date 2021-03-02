e4bef1f4c7775aa18d9574ced3fea8f6b344c345
Marina Anisimova <anisimova.marina.n@gmail.com>
2021-03-02 23:58:04 +0300

    feature: сделать лого в header ссылкой

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import headerLogo from '../../images/logo-header.svg';
 import Navigation from '../Navigation/Navigation';
+import { Link } from 'react-router-dom';
 
 function Header (props) {
   return (
     <header className="header">
-      <div style={{ backgroundImage: `url(${headerLogo})` }} className="header__logo" />
-     <Navigation toggleClass={props.toggleClass} visible={props.visible} />
+      <Link style={{ backgroundImage: `url(${headerLogo})` }} className="header__logo" />
+      <Navigation toggleClass={props.toggleClass} visible={props.visible} />
     </header>
   )
 }
